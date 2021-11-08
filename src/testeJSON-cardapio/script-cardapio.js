7a4c993004b666999fd186f2c1e944a2c4e691da
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-08 20:25:40 -0300

    adicionar produtos pelo javascript

--- src/testeJSON-cardapio/script-cardapio.js
+++ src/testeJSON-cardapio/script-cardapio.js
@@ -1,20 +1,21 @@
 // INSERIR PRODUTOS COM JAVASCRIPT
 
-var listaProdutos = ''
+// LANCHES
+var listaProdutosLanches = ''
 
-for (i = 0; i < db.dados.length; i++) {
-  var produto = `
+for (i = 0; i < db.lanches.length; i++) {
+  let produto = `
   <div class="box-produto">
     <div class="image">
-      <img src="${db.dados[i].imagem}" alt="${db.dados[i].titulo}"/>
+      <img src="${db.lanches[i].imagem}" alt="${db.lanches[i].titulo}"/>
     </div>
     <div class="produto">
       <div>
-        <h3 class="nome-produto">${db.dados[i].titulo}</h3>
-        <p class="descricao-produto">${db.dados[i].descricao}</p>
+        <h3 class="nome-produto">${db.lanches[i].titulo}</h3>
+        <p class="descricao-produto">${db.lanches[i].descricao}</p>
       </div>
       <div class="precos">
-        <p class="preco-novo-produto">R$ ${db.dados[i].preco}</p>
+        <p class="preco-novo-produto">R$ ${db.lanches[i].preco}</p>
       </div>
     </div>
     <div>
@@ -24,10 +25,112 @@ for (i = 0; i < db.dados.length; i++) {
     </div>
   </div>
   `
-  listaProdutos += produto
+  listaProdutosLanches += produto
 }
 
-document.getElementById('produtosJSON').innerHTML = listaProdutos
+document.getElementById('produtosJavaScriptLanches').innerHTML =
+  listaProdutosLanches
+
+//===================================================================
+
+// LANCHES FIT
+var listaProdutosLanchesFit = ''
+
+for (i = 0; i < db.lanchesFit.length; i++) {
+  let produto = `
+    <div class="box-produto">
+      <div class="image">
+        <img src="${db.lanchesFit[i].imagem}" alt="${db.lanchesFit[i].titulo}"/>
+      </div>
+      <div class="produto">
+        <div>
+          <h3 class="nome-produto">${db.lanchesFit[i].titulo}</h3>
+          <p class="descricao-produto">${db.lanchesFit[i].descricao}</p>
+        </div>
+        <div class="precos">
+          <p class="preco-novo-produto">R$ ${db.lanchesFit[i].preco}</p>
+        </div>
+      </div>
+      <div>
+        <a href="#">
+          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+        </a>
+      </div>
+    </div>
+    `
+  listaProdutosLanchesFit += produto
+}
+
+document.getElementById('produtosJavaScriptLanchesFit').innerHTML =
+  listaProdutosLanchesFit
+
+//===================================================================
+
+// BEBIDAS
+var listaProdutosBebidas = ''
+
+for (i = 0; i < db.bebidas.length; i++) {
+  let produto = `
+  <div class="box-produto">
+    <div class="image-vertical">
+      <img src="${db.bebidas[i].imagem}" alt="${db.bebidas[i].titulo}"/>
+    </div>
+    <div class="produto">
+      <div>
+        <h3 class="nome-produto">${db.bebidas[i].titulo}</h3>
+        <p class="descricao-produto">${db.bebidas[i].descricao}</p>
+      </div>
+      <div class="precos">
+        <p class="preco-novo-produto">R$ ${db.bebidas[i].preco}</p>
+      </div>
+    </div>
+    <div>
+      <a href="#">
+        <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+      </a>
+    </div>
+  </div>
+  `
+  listaProdutosBebidas += produto
+}
+
+document.getElementById('produtosJavaScriptBebidas').innerHTML =
+  listaProdutosBebidas
+
+//===================================================================
+
+// SOBREMESAS
+var listaProdutosSobremesas = ''
+
+for (i = 0; i < db.sobremesas.length; i++) {
+  let produto = `
+  <div class="box-produto">
+    <div class="image-vertical">
+      <img src="${db.sobremesas[i].imagem}" alt="${db.sobremesas[i].titulo}"/>
+    </div>
+    <div class="produto">
+      <div>
+        <h3 class="nome-produto">${db.sobremesas[i].titulo}</h3>
+        <p class="descricao-produto">${db.sobremesas[i].descricao}</p>
+      </div>
+      <div class="precos">
+        <p class="preco-novo-produto">R$ ${db.sobremesas[i].preco}</p>
+      </div>
+    </div>
+    <div>
+      <a href="#">
+        <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+      </a>
+    </div>
+  </div>
+  `
+  listaProdutosSobremesas += produto
+}
+
+document.getElementById('produtosJavaScriptSobremesas').innerHTML =
+  listaProdutosSobremesas
+
+//===================================================================
 
 // API CLIENTES
 
@@ -42,7 +145,7 @@ fetch(url)
   })
 
 function inserirDados(data) {
-  var listaClientes = ''
+  let listaClientes = ''
   let dados = data.results
   // console.log(data.results)
   // "data.results" é um array que possui o objeto de cada pessoa
