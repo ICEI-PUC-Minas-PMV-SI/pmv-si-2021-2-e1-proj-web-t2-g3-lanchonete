cbc01e3cc4eeb55e9fda69dcd3b17f37aef8fb8b
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-10 11:23:57 -0300

    criando json-server

--- src/testeJSON-cardapio/script-cardapio.js
+++ src/testeJSON-cardapio/script-cardapio.js
@@ -1,64 +1,37 @@
 // INSERIR PRODUTOS COM JAVASCRIPT
 
 // PEGAR ARQUIVO JSON
-// const urlJSON = ``
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data)
-//   })
+const urlCardapioJSON = `http://localhost:3000/cardapio`
 
-// LANCHES
-var listaProdutosLanches = ''
-var arrayLanches = db.cardapio[0].lanches
+fetch(urlCardapioJSON)
+  .then(response => response.json())
+  .then(listaProdutos => {
+    inserirProdutos(listaProdutos)
+  })
 
-for (i = 0; i < arrayLanches.length; i++) {
-  let produto = `
-  <div class="box-produto">
-    <div class="image">
-      <img src="${arrayLanches[i].imagem}" alt="${arrayLanches.titulo}"/>
-    </div>
-    <div class="produto">
-      <div>
-        <h3 class="nome-produto">${arrayLanches[i].titulo}</h3>
-        <p class="descricao-produto">${arrayLanches[i].descricao}</p>
-      </div>
-      <div class="precos">
-        <p class="preco-novo-produto">R$ ${arrayLanches[i].preco}</p>
-      </div>
-    </div>
-    <div>
-      <a href="#">
-        <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-      </a>
-    </div>
-  </div>
-  `
-  listaProdutosLanches += produto
-}
+function inserirProdutos(produtos) {
+  // console.log(produtos)
+  var arrayLanches = produtos[0].lanches
+  var arrayLanchesFit = produtos[1].lanchesFit
+  var arrayBebidas = produtos[2].bebidas
+  var arraySobremesas = produtos[3].sobremesas
 
-document.getElementById('produtosJavaScriptLanches').innerHTML =
-  listaProdutosLanches
+  // ADICIONAR LANCHES
+  var listaProdutosLanches = ''
 
-//===================================================================
-
-// LANCHES FIT
-var listaProdutosLanchesFit = ''
-var arrayLanchesFit = db.cardapio[1].lanchesFit
-
-for (i = 0; i < arrayLanchesFit.length; i++) {
-  let produto = `
+  for (i = 0; i < arrayLanches.length; i++) {
+    let produto = `
     <div class="box-produto">
       <div class="image">
-        <img src="${arrayLanchesFit[i].imagem}" alt="${arrayLanchesFit[i].titulo}"/>
+        <img src="${arrayLanches[i].imagem}" alt="${arrayLanches[i].titulo}"/>
       </div>
       <div class="produto">
         <div>
-          <h3 class="nome-produto">${arrayLanchesFit[i].titulo}</h3>
-          <p class="descricao-produto">${arrayLanchesFit[i].descricao}</p>
+          <h3 class="nome-produto">${arrayLanches[i].titulo}</h3>
+          <p class="descricao-produto">${arrayLanches[i].descricao}</p>
         </div>
         <div class="precos">
-          <p class="preco-novo-produto">R$ ${arrayLanchesFit[i].preco}</p>
+          <p class="preco-novo-produto">R$ ${arrayLanches[i].preco}</p>
         </div>
       </div>
       <div>
@@ -68,87 +41,119 @@ for (i = 0; i < arrayLanchesFit.length; i++) {
       </div>
     </div>
     `
-  listaProdutosLanchesFit += produto
-}
+    listaProdutosLanches += produto
+  }
 
-document.getElementById('produtosJavaScriptLanchesFit').innerHTML =
-  listaProdutosLanchesFit
+  document.getElementById('produtosJavaScriptLanches').innerHTML =
+    listaProdutosLanches
 
-//===================================================================
+  //===================================================================
 
-// BEBIDAS
-var listaProdutosBebidas = ''
-var arrayBebidas = db.cardapio[2].bebidas
+  // ADICIONAR LANCHES FIT
+  var listaProdutosLanchesFit = ''
 
-for (i = 0; i < arrayBebidas.length; i++) {
-  let produto = `
-  <div class="box-produto">
-    <div class="image-vertical">
-      <img src="${arrayBebidas[i].imagem}" alt="${arrayBebidas[i].titulo}"/>
-    </div>
-    <div class="produto">
+  for (i = 0; i < arrayLanchesFit.length; i++) {
+    let produto = `
+      <div class="box-produto">
+        <div class="image">
+          <img src="${arrayLanchesFit[i].imagem}" alt="${arrayLanchesFit[i].titulo}"/>
+        </div>
+        <div class="produto">
+          <div>
+            <h3 class="nome-produto">${arrayLanchesFit[i].titulo}</h3>
+            <p class="descricao-produto">${arrayLanchesFit[i].descricao}</p>
+          </div>
+          <div class="precos">
+            <p class="preco-novo-produto">R$ ${arrayLanchesFit[i].preco}</p>
+          </div>
+        </div>
+        <div>
+          <a href="#">
+            <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+          </a>
+        </div>
+      </div>
+      `
+    listaProdutosLanchesFit += produto
+  }
+
+  document.getElementById('produtosJavaScriptLanchesFit').innerHTML =
+    listaProdutosLanchesFit
+
+  //===================================================================
+
+  // ADICIONAR BEBIDAS
+  var listaProdutosBebidas = ''
+
+  for (i = 0; i < arrayBebidas.length; i++) {
+    let produto = `
+    <div class="box-produto">
+      <div class="image-vertical">
+        <img src="${arrayBebidas[i].imagem}" alt="${arrayBebidas[i].titulo}"/>
+      </div>
+      <div class="produto">
+        <div>
+          <h3 class="nome-produto">${arrayBebidas[i].titulo}</h3>
+          <p class="descricao-produto">${arrayBebidas[i].descricao}</p>
+        </div>
+        <div class="precos">
+          <p class="preco-novo-produto">R$ ${arrayBebidas[i].preco}</p>
+        </div>
+      </div>
       <div>
-        <h3 class="nome-produto">${arrayBebidas[i].titulo}</h3>
-        <p class="descricao-produto">${arrayBebidas[i].descricao}</p>
-      </div>
-      <div class="precos">
-        <p class="preco-novo-produto">R$ ${arrayBebidas[i].preco}</p>
+        <a href="#">
+          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+        </a>
       </div>
     </div>
-    <div>
-      <a href="#">
-        <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-      </a>
-    </div>
-  </div>
-  `
-  listaProdutosBebidas += produto
-}
+    `
+    listaProdutosBebidas += produto
+  }
 
-document.getElementById('produtosJavaScriptBebidas').innerHTML =
-  listaProdutosBebidas
+  document.getElementById('produtosJavaScriptBebidas').innerHTML =
+    listaProdutosBebidas
 
-//===================================================================
+  //===================================================================
 
-// SOBREMESAS
-var listaProdutosSobremesas = ''
-var arraySobremesas = db.cardapio[3].sobremesas
+  // ADICIONAR SOBREMESAS
+  var listaProdutosSobremesas = ''
 
-for (i = 0; i < arraySobremesas.length; i++) {
-  let produto = `
-  <div class="box-produto">
-    <div class="image-vertical">
-      <img src="${arraySobremesas[i].imagem}" alt="${arraySobremesas[i].titulo}"/>
-    </div>
-    <div class="produto">
+  for (i = 0; i < arraySobremesas.length; i++) {
+    let produto = `
+    <div class="box-produto">
+      <div class="image-vertical">
+        <img src="${arraySobremesas[i].imagem}" alt="${arraySobremesas[i].titulo}"/>
+      </div>
+      <div class="produto">
+        <div>
+          <h3 class="nome-produto">${arraySobremesas[i].titulo}</h3>
+          <p class="descricao-produto">${arraySobremesas[i].descricao}</p>
+        </div>
+        <div class="precos">
+          <p class="preco-novo-produto">R$ ${arraySobremesas[i].preco}</p>
+        </div>
+      </div>
       <div>
-        <h3 class="nome-produto">${arraySobremesas[i].titulo}</h3>
-        <p class="descricao-produto">${arraySobremesas[i].descricao}</p>
-      </div>
-      <div class="precos">
-        <p class="preco-novo-produto">R$ ${arraySobremesas[i].preco}</p>
+        <a href="#">
+          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+        </a>
       </div>
     </div>
-    <div>
-      <a href="#">
-        <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-      </a>
-    </div>
-  </div>
-  `
-  listaProdutosSobremesas += produto
-}
+    `
+    listaProdutosSobremesas += produto
+  }
 
-document.getElementById('produtosJavaScriptSobremesas').innerHTML =
-  listaProdutosSobremesas
+  document.getElementById('produtosJavaScriptSobremesas').innerHTML =
+    listaProdutosSobremesas
+}
 
 //===================================================================
 
 // API CLIENTES
 
-const url = `https://randomuser.me/api/?results=12`
+const urlUsuarios = `https://randomuser.me/api/?results=12`
 
-fetch(url)
+fetch(urlUsuarios)
   .then(response => response.json())
   .then(dados => {
     // console.log(dados)
