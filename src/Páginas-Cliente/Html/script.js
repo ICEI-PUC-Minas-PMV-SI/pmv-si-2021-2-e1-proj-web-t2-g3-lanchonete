51a5cc53ce17fb3c7850701980b8569814714424
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-11 15:36:09 -0300

    adicionar modal acessar como

---
+++ src/Páginas-Cliente/Html/script.js
@@ -0,0 +1,9 @@
+function modal() {
+  let modal = document.getElementById('modal-acessar-como')
+  modal.style.opacity = '1'
+}
+
+function fechar() {
+  let modal = document.getElementById('modal-acessar-como')
+  modal.style.opacity = '0'
+}
