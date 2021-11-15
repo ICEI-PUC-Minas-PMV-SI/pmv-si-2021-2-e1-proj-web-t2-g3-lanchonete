241debe33e7e8d59f08e31a9ee587d5db827bd8c
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-15 19:39:04 -0300

    deletar, editar e remover produtos

--- src/Páginas-Admin/cadastro-produtos/main.js
+++ src/Páginas-Admin/cadastro-produtos/main.js
@@ -1,26 +1,31 @@
-URL = 'http://localhost:3000/cardapio'
+URL = 'http://localhost:3008/cardapio'
 //=================================================================================================
 
 // GET - Recupera todos os produtos e adiciona na tabela
 
-const promocaoList = document.getElementById('promocao-list')
+const produtoList = document.getElementById('produto-list')
 
 fetch(URL)
   .then(res => res.json())
   .then(produtos => {
-    let lista_promocoes = ''
+    let lista_produtos = ''
 
     for (let i = 0; i < produtos.length; i++) {
-      if (produtos[i].categoria == 'Promoções') {
-        lista_promocoes += `
+      if (
+        produtos[i].categoria.toUpperCase() == 'LANCHES' ||
+        produtos[i].categoria.toUpperCase() == 'LANCHES FIT' ||
+        produtos[i].categoria.toUpperCase() == 'BEBIDAS' ||
+        produtos[i].categoria.toUpperCase() == 'SOBREMESAS'
+      ) {
+        lista_produtos += `
               <tr>
                   <th>${produtos[i].categoria}</th>
                   <th>${produtos[i].id}</th>
                   <td>${produtos[i].titulo}</td>
                   <td>${produtos[i].descricao}</td>
                   <td>${produtos[i].imagem}</td>
-                  <td>R$${parseFloat(produtos[i].precoAntigo).toFixed(2)}</td>
-                  <td>R$${parseFloat(produtos[i].precoNovo).toFixed(2)}</td>
+                  <td>R$${parseFloat(produtos[i].preco).toFixed(2)}</td>
+                  <td>${parseInt(produtos[i].quantidade)}</td>
                   <td>
                       <a onclick="getProduto(${produtos[i].id});" 
                       class="btn btn-warning btn-sm" 
@@ -37,7 +42,7 @@ fetch(URL)
                   </td>
               </tr>
               `
-        promocaoList.innerHTML = lista_promocoes
+        produtoList.innerHTML = lista_produtos
       }
     }
   })
@@ -58,35 +63,76 @@ produtoDelete.addEventListener('click', e => {
 })
 //=================================================================================================
 
-// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UMA PROMOÇÃO NA API
-
+// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UM PRODUTO NA API
 function getProduto(id) {
   if (id == 0) {
     $('#edit-prod-id').text('')
     $('#produto-id').prop('disabled', false)
-    $('#produto-categoria').val('Promoção').prop('disabled', true)
+    $('#produto-categoria').val('')
     $('#produto-id').val('')
     $('#produto-nome').val('')
     $('#produto-descricao').val('')
     $('#produto-imagem').val('')
-    $('#produto-precoAntigo').val('')
-    $('#produto-precoNovo').val('')
+    $('#produto-preco').val('')
+    $('#produto-quantidade').val('')
   } else {
     $('#edit-prod-id').text(id)
     fetch(`${URL}/${id}`)
       .then(res => res.json())
       .then(data => {
         $('#produto-id').prop('disabled', true)
-        $('#produto-categoria').prop('disabled', true)
         $('#produto-categoria').val(data.categoria)
         $('#produto-id').val(data.id)
         $('#produto-nome').val(data.titulo)
         $('#produto-descricao').val(data.descricao)
         $('#produto-imagem').val(data.imagem)
-        $('#produto-precoAntigo').val(parseFloat(data.precoAntigo).toFixed(2))
-        $('#produto-precoNovo').val(parseFloat(data.precoNovo).toFixed(2))
+        $('#produto-preco').val(parseFloat(data.preco).toFixed(2))
+        $('#produto-quantidade').val(data.quantidade)
       })
   }
 }
 
+//=================================================================================================
+
+// CREATE or UPDATE - PROCEDIMENTO PARA CRIAR OU EDITAR UM PRODUTO
+
+const produtoForm = document.getElementById('produto-form')
+
+produtoForm.addEventListener('submit', e => {
+  // RECUPERA O ID DO PRODUTO
+  let id = parseInt($('#edit-prod-id').text())
+
+  // RECUPERA OS DADOS DO PRODUTO
+  const produto = JSON.stringify({
+    categoria: document.getElementById('produto-categoria').value,
+    id: document.getElementById('produto-id').value,
+    nome: document.getElementById('produto-nome').value,
+    descricao: document.getElementById('produto-descricao').value,
+    imagem: document.getElementById('produto-imagem').value,
+    preco: document.getElementById('produto-preco').value,
+    quantidade: document.getElementById('produto-quantidade').value
+  })
+
+  if (id >= 0) {
+    fetch(`${URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: produto
+    })
+      .then(res => res.json())
+      .then(() => location.reload())
+  } else {
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: produto
+    })
+      .then(res => res.json())
+      .then(() => location.reload())
+  }
+})
 //=================================================================================================
--- src/Páginas-Admin/cadastro-promocoes/main.js
+++ src/Páginas-Admin/cadastro-promocoes/main.js
@@ -1,31 +1,26 @@
-URL = 'http://localhost:3000/cardapio'
+URL = 'http://localhost:3008/cardapio'
 //=================================================================================================
 
 // GET - Recupera todos os produtos e adiciona na tabela
 
-const produtoList = document.getElementById('produto-list')
+const promocaoList = document.getElementById('promocao-list')
 
 fetch(URL)
   .then(res => res.json())
   .then(produtos => {
-    let lista_produtos = ''
+    let lista_promocoes = ''
 
     for (let i = 0; i < produtos.length; i++) {
-      if (
-        produtos[i].categoria == 'Lanches' ||
-        produtos[i].categoria == 'Lanches Fit' ||
-        produtos[i].categoria == 'Bebidas' ||
-        produtos[i].categoria == 'Sobremesas'
-      ) {
-        lista_produtos += `
+      if (produtos[i].categoria.toUpperCase() == 'PROMOÇÕES') {
+        lista_promocoes += `
               <tr>
                   <th>${produtos[i].categoria}</th>
                   <th>${produtos[i].id}</th>
                   <td>${produtos[i].titulo}</td>
                   <td>${produtos[i].descricao}</td>
                   <td>${produtos[i].imagem}</td>
-                  <td>R$${parseFloat(produtos[i].preco).toFixed(2)}</td>
-                  <td>${parseInt(produtos[i].quantidade)}</td>
+                  <td>R$${parseFloat(produtos[i].precoAntigo).toFixed(2)}</td>
+                  <td>R$${parseFloat(produtos[i].precoNovo).toFixed(2)}</td>
                   <td>
                       <a onclick="getProduto(${produtos[i].id});" 
                       class="btn btn-warning btn-sm" 
@@ -42,7 +37,7 @@ fetch(URL)
                   </td>
               </tr>
               `
-        produtoList.innerHTML = lista_produtos
+        promocaoList.innerHTML = lista_promocoes
       }
     }
   })
@@ -63,33 +58,78 @@ produtoDelete.addEventListener('click', e => {
 })
 //=================================================================================================
 
-// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UM PRODUTO NA API
+// PROCEDIMENTO PARA RECUPERAR OS DADOS DE UMA PROMOÇÃO NA API
+
 function getProduto(id) {
   if (id == 0) {
     $('#edit-prod-id').text('')
     $('#produto-id').prop('disabled', false)
-    $('#produto-categoria').val('')
+    $('#produto-categoria').val('Promoção').prop('disabled', true)
     $('#produto-id').val('')
     $('#produto-nome').val('')
     $('#produto-descricao').val('')
     $('#produto-imagem').val('')
-    $('#produto-preco').val('')
-    $('#produto-quantidade').val('')
+    $('#produto-precoAntigo').val('')
+    $('#produto-precoNovo').val('')
   } else {
     $('#edit-prod-id').text(id)
     fetch(`${URL}/${id}`)
       .then(res => res.json())
       .then(data => {
         $('#produto-id').prop('disabled', true)
+        $('#produto-categoria').prop('disabled', true)
         $('#produto-categoria').val(data.categoria)
         $('#produto-id').val(data.id)
         $('#produto-nome').val(data.titulo)
         $('#produto-descricao').val(data.descricao)
         $('#produto-imagem').val(data.imagem)
-        $('#produto-preco').val(parseFloat(data.preco).toFixed(2))
-        $('#produto-quantidade').val(data.quantidade)
+        $('#produto-precoAntigo').val(parseFloat(data.precoAntigo).toFixed(2))
+        $('#produto-precoNovo').val(parseFloat(data.precoNovo).toFixed(2))
       })
   }
 }
 
+//=================================================================================================
+
+// CREATE or UPDATE - PROCEDIMENTO PARA CRIAR OU EDITAR UM PRODUTO
+
+const produtoForm = document.getElementById('produto-form')
+
+produtoForm.addEventListener('submit', e => {
+  // RECUPERA O ID DO PRODUTO
+  let id = parseInt($('#edit-prod-id').text())
+
+  // RECUPERA OS DADOS DO PRODUTO
+  const produto = JSON.stringify({
+    categoria: document.getElementById('produto-categoria').value,
+    id: document.getElementById('produto-id').value,
+    nome: document.getElementById('produto-nome').value,
+    descricao: document.getElementById('produto-descricao').value,
+    imagem: document.getElementById('produto-imagem').value,
+    precoAntigo: document.getElementById('produto-precoAntigo').value,
+    precoNovo: document.getElementById('produto-precoNovo').value
+  })
+
+  if (id >= 0) {
+    fetch(`${URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: produto
+    })
+      .then(res => res.json())
+      .then(() => location.reload())
+  } else {
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: produto
+    })
+      .then(res => res.json())
+      .then(() => location.reload())
+  }
+})
 //=================================================================================================
