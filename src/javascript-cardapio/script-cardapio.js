b99beb02a10050f555167b98ab390aab4396a9d9
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-15 18:59:01 -0300

    página cadastro de produtos e de promoções

--- src/javascript-cardapio/script-cardapio.js
+++ src/javascript-cardapio/script-cardapio.js
@@ -10,61 +10,30 @@ fetch(urlCardapioJSON)
 // INSERIR PRODUTOS COM JAVASCRIPT
 
 function inserirProdutos(produtos) {
-  console.log(produtos)
-  var arrayLanches = produtos[0].lanches
-  var arrayLanchesFit = produtos[1].lanchesFit
-  var arrayBebidas = produtos[2].bebidas
-  var arraySobremesas = produtos[3].sobremesas
-  var arrayPromocoes = produtos[4].promocoes
+  // console.log(produtos)
 
-  // ADICIONAR LANCHES
   var listaProdutosLanches = ''
-
-  for (i = 0; i < arrayLanches.length; i++) {
-    let produto = `
-    <div class="box-produto">
-      <div class="image">
-        <img src="${arrayLanches[i].imagem}" alt="${arrayLanches[i].titulo}"/>
-      </div>
-      <div class="produto">
-        <div>
-          <h3 class="nome-produto">${arrayLanches[i].titulo}</h3>
-          <p class="descricao-produto">${arrayLanches[i].descricao}</p>
-        </div>
-        <div class="precos">
-          <p class="preco-novo-produto">R$ ${arrayLanches[i].preco}</p>
-        </div>
-      </div>
-      <div>
-        <a href="#">
-          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-        </a>
-      </div>
-    </div>
-    `
-    listaProdutosLanches += produto
-  }
-
-  document.getElementById('produtosLanches').innerHTML = listaProdutosLanches
-
-  //===================================================================
-
-  // ADICIONAR LANCHES FIT
   var listaProdutosLanchesFit = ''
+  var listaProdutosBebidas = ''
+  var listaProdutosSobremesas = ''
+  var listaProdutosPromocoes = ''
 
-  for (i = 0; i < arrayLanchesFit.length; i++) {
-    let produto = `
+  for (i = 0; i < produtos.length; i++) {
+    // ADICIONAR LANCHES
+
+    if (produtos[i].categoria == 'Lanches') {
+      let produto = `
       <div class="box-produto">
         <div class="image">
-          <img src="${arrayLanchesFit[i].imagem}" alt="${arrayLanchesFit[i].titulo}"/>
+          <img src="${produtos[i].imagem}" alt="${produtos[i].titulo}"/>
         </div>
         <div class="produto">
           <div>
-            <h3 class="nome-produto">${arrayLanchesFit[i].titulo}</h3>
-            <p class="descricao-produto">${arrayLanchesFit[i].descricao}</p>
+            <h3 class="nome-produto">${produtos[i].titulo}</h3>
+            <p class="descricao-produto">${produtos[i].descricao}</p>
           </div>
           <div class="precos">
-            <p class="preco-novo-produto">R$ ${arrayLanchesFit[i].preco}</p>
+            <p class="preco-novo-produto">R$ ${produtos[i].preco}</p>
           </div>
         </div>
         <div>
@@ -74,108 +43,125 @@ function inserirProdutos(produtos) {
         </div>
       </div>
       `
-    listaProdutosLanchesFit += produto
-  }
+      listaProdutosLanches += produto
+    }
 
-  document.getElementById('produtosLanchesFit').innerHTML =
-    listaProdutosLanchesFit
+    // ADICIONAR LANCHES FIT
 
-  //===================================================================
-
-  // ADICIONAR BEBIDAS
-  var listaProdutosBebidas = ''
-
-  for (i = 0; i < arrayBebidas.length; i++) {
-    let produto = `
-    <div class="box-produto">
-      <div class="image-vertical">
-        <img src="${arrayBebidas[i].imagem}" alt="${arrayBebidas[i].titulo}"/>
-      </div>
-      <div class="produto">
+    if (produtos[i].categoria == 'Lanches Fit') {
+      let produto = `
+      <div class="box-produto">
+        <div class="image">
+          <img src="${produtos[i].imagem}" alt="${produtos[i].titulo}"/>
+        </div>
+        <div class="produto">
+          <div>
+            <h3 class="nome-produto">${produtos[i].titulo}</h3>
+            <p class="descricao-produto">${produtos[i].descricao}</p>
+          </div>
+          <div class="precos">
+            <p class="preco-novo-produto">R$ ${produtos[i].preco}</p>
+          </div>
+        </div>
         <div>
-          <h3 class="nome-produto">${arrayBebidas[i].titulo}</h3>
-          <p class="descricao-produto">${arrayBebidas[i].descricao}</p>
-        </div>
-        <div class="precos">
-          <p class="preco-novo-produto">R$ ${arrayBebidas[i].preco}</p>
+          <a href="#">
+            <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+          </a>
         </div>
       </div>
-      <div>
-        <a href="#">
-          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-        </a>
-      </div>
-    </div>
-    `
-    listaProdutosBebidas += produto
-  }
+      `
+      listaProdutosLanchesFit += produto
+    }
 
-  document.getElementById('produtosBebidas').innerHTML = listaProdutosBebidas
+    // ADICIONAR BEBIDAS
 
-  //===================================================================
-
-  // ADICIONAR SOBREMESAS
-  var listaProdutosSobremesas = ''
-
-  for (i = 0; i < arraySobremesas.length; i++) {
-    let produto = `
-    <div class="box-produto">
-      <div class="image-vertical">
-        <img src="${arraySobremesas[i].imagem}" alt="${arraySobremesas[i].titulo}"/>
-      </div>
-      <div class="produto">
+    if (produtos[i].categoria == 'Bebidas') {
+      let produto = `
+      <div class="box-produto">
+        <div class="image-vertical">
+          <img src="${produtos[i].imagem}" alt="${produtos[i].titulo}"/>
+        </div>
+        <div class="produto">
+          <div>
+            <h3 class="nome-produto">${produtos[i].titulo}</h3>
+            <p class="descricao-produto">${produtos[i].descricao}</p>
+          </div>
+          <div class="precos">
+            <p class="preco-novo-produto">R$ ${produtos[i].preco}</p>
+          </div>
+        </div>
         <div>
-          <h3 class="nome-produto">${arraySobremesas[i].titulo}</h3>
-          <p class="descricao-produto">${arraySobremesas[i].descricao}</p>
-        </div>
-        <div class="precos">
-          <p class="preco-novo-produto">R$ ${arraySobremesas[i].preco}</p>
+          <a href="#">
+            <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+          </a>
         </div>
       </div>
-      <div>
-        <a href="#">
-          <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
-        </a>
+      `
+      listaProdutosBebidas += produto
+    }
+
+    // ADICIONAR SOBREMESAS
+
+    if (produtos[i].categoria == 'Sobremesas') {
+      let produto = `
+      <div class="box-produto">
+        <div class="image-vertical">
+          <img src="${produtos[i].imagem}" alt="${produtos[i].titulo}"/>
+        </div>
+        <div class="produto">
+          <div>
+            <h3 class="nome-produto">${produtos[i].titulo}</h3>
+            <p class="descricao-produto">${produtos[i].descricao}</p>
+          </div>
+          <div class="precos">
+            <p class="preco-novo-produto">R$ ${produtos[i].preco}</p>
+          </div>
+        </div>
+        <div>
+          <a href="#">
+            <img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon" />
+          </a>
+        </div>
       </div>
-    </div>
-    `
-    listaProdutosSobremesas += produto
-  }
+      `
+      listaProdutosSobremesas += produto
+    }
 
-  document.getElementById('produtosSobremesas').innerHTML =
-    listaProdutosSobremesas
+    // ADICIONAR PROMOÇÕES
 
-  //===================================================================
-
-  // ADICIONAR PROMOÇÕES
-  var listaProdutosPromocoes = ''
-
-  for (i = 0; i < arrayPromocoes.length; i++) {
-    let produto = `
-    <div class="box-produto">
+    if (produtos[i].categoria == 'Promoções') {
+      let produto = `
+      <div class="box-produto">
       <div class="image">
-        <img src="${arrayPromocoes[i].imagem}" alt="${arrayPromocoes[i].titulo}"/>
+        <img src="${produtos[i].imagem}" alt="${produtos[i].titulo}"/>
       </div>
       <div class="produto">
         <div>
-          <h3 class="nome-produto">${arrayPromocoes[i].titulo}</h3>
-          <p class="descricao-produto">${arrayPromocoes[i].descricao}
+          <h3 class="nome-produto">${produtos[i].titulo}</h3>
+          <p class="descricao-produto">${produtos[i].descricao}
           </p>
         </div>
         <div class="precos">
           <div class="caixa-preco-antigo">
             <img src="../midia/cardapio/icons/line-icon.png" alt="Ícone linha" class="line-icon"/>
-            <p class="preco-antigo-produto">R$ ${arrayPromocoes[i].precoAntigo}</p>
+            <p class="preco-antigo-produto">R$ ${produtos[i].precoAntigo}</p>
           </div>
-          <p class="preco-novo-produto">R$ ${arrayPromocoes[i].precoNovo}</p>
+          <p class="preco-novo-produto">R$ ${produtos[i].precoNovo}</p>
         </div>
       </div>
       <a href="#"><img src="../midia/cardapio/icons/plus-icon.png" alt="Ícone adicionar" class="plus-icon"/></a>
     </div>
-    `
-    listaProdutosPromocoes += produto
+      `
+      listaProdutosPromocoes += produto
+    }
   }
 
+  document.getElementById('produtosLanches').innerHTML = listaProdutosLanches
+  document.getElementById('produtosLanchesFit').innerHTML =
+    listaProdutosLanchesFit
+  document.getElementById('produtosBebidas').innerHTML = listaProdutosBebidas
+  document.getElementById('produtosSobremesas').innerHTML =
+    listaProdutosSobremesas
   document.getElementById('produtosPromocoes').innerHTML =
     listaProdutosPromocoes
 }
