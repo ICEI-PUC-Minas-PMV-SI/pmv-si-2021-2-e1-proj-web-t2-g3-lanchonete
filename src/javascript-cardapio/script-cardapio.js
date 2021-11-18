885a950fd3fe21f56b4af5897028ad8d4a1b2855
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-18 08:41:37 -0300

    finalizar selecionar produtos e numero carrinho

--- src/javascript-cardapio/script-cardapio.js
+++ src/javascript-cardapio/script-cardapio.js
@@ -156,53 +156,100 @@ function inserirProdutos(produtos) {
     listaProdutosSobremesas
   document.getElementById('produtosPromocoes').innerHTML =
     listaProdutosPromocoes
+  // Adiciona os produtos no HTML em suas respectivas categorias (lanches, lanches fit, bebidas ou sobremesas)
 }
 
+// ==========================================================================================================================
+
+// Código para selecionar produtos e salvar os produtos selecionados no localStorage, além de exibir a quantidade de produtos selecionados
+
 let numeroCarrinho = document.getElementById('numero-carrinho')
+let qtdProdutosSelecionados = localStorage.getItem('numero_display_carrinho')
+
+if (qtdProdutosSelecionados) {
+  // Se o localStorage conter o item numero_display_carrinho, pega-se o que está armazenado nele (a quantidade de produtos selecionados) e coloca lá em cima do carrinho
+
+  numeroCarrinho.innerHTML = qtdProdutosSelecionados
+  exibirNumeroCarrinho()
+}
+
+function exibirNumeroCarrinho() {
+  numeroCarrinho.style.display = 'block'
+}
 
 function selecionarProduto(nome, preco) {
   if (localStorage.getItem('db_produtosSelecionados')) {
+    // Se o localStorage conter o item db_produtosSelecionados, armazena-se o conteúdo dele (transformado para objeto) na variável array_produtosSelecionados
+
     let array_produtosSelecionados = JSON.parse(
       localStorage.getItem('db_produtosSelecionados')
     )
 
-    numeroCarrinho.innerHTML = array_produtosSelecionados.length + 1
-    numeroCarrinho.style.display = 'block'
+    qtdProdutosSelecionados = array_produtosSelecionados.length + 1
+    // qtdProdutosSelecionados recebe o número de produtos armazenados no localStorage
 
-    let produtosSelecionados = {
+    numeroCarrinho.innerHTML = qtdProdutosSelecionados
+    exibirNumeroCarrinho()
+    // Exibe o número de produtos do array lá no carrinho
+
+    localStorage.setItem('numero_display_carrinho', qtdProdutosSelecionados)
+    // Armazena no local storage o numero que está lá no carrinho
+
+    let produtoSelecionado = {
       nomeProduto: nome,
       precoProduto: preco
     }
+    // Objeto para armazenar o nome e preço do produto selecionado pelo cliente
 
-    array_produtosSelecionados.push(produtosSelecionados)
+    array_produtosSelecionados.push(produtoSelecionado)
+    // Adiciona o novo produto selecionado no array de produtos selecionados
 
     localStorage.setItem(
       'db_produtosSelecionados',
       JSON.stringify(array_produtosSelecionados)
     )
+    // Atualiza-se o localStorage do db_produtosSelecionados contendo o novo produto selecionado
+    // "JSON.stringify" porque o localStorage só aceita string, e esse método transforma objeto em string JSON
+
+    // !!!!!!! IMPORTANTE !!!!!!!
+    // Na primeira vez que o cliente acessar a página e for adicionar um produto não vai ter db_produtosSelecionados no localStorage, portanto, vai executar o código abaixo (do "else"). Já na segunda vez que o cliente for adicionar outro produto vai executar o código acima, pois já terá um db_produtosSelecionados. Isso tudo foi pensado para que, caso o cliente der um refresh na página, não irá iniciar um array vazio de produtos, ou seja, ele não irá perder os produtos selecionados, pois os mesmos foram armazenados no db_produtosSelecionados
+
+    //
   } else {
     let array_produtosSelecionados = []
+    // Caso o localStorage não contenha o item db_produtosSelecionados, ou seja, caso o cliente não tenha selecionado nada ainda, inicia-se um array vazio para armazenar os produtos
 
-    numeroCarrinho.innerHTML = 1
-    numeroCarrinho.style.display = 'block'
-
-    let produtosSelecionados = {
+    let produtoSelecionado = {
       nomeProduto: nome,
       precoProduto: preco
     }
+    // Objeto para armazenar o nome e preço do produto selecionado pelo cliente
 
-    array_produtosSelecionados.push(produtosSelecionados)
+    array_produtosSelecionados.push(produtoSelecionado)
+    // Adiciona o objeto do produto selecionado no array
 
     localStorage.setItem(
       'db_produtosSelecionados',
       JSON.stringify(array_produtosSelecionados)
     )
+    // Adiciona no localStorage o db_produtosSelecionados que contém o array com os produtos selecionados pelo cliente
+
+    qtdProdutosSelecionados = 1
+    // Como é o primeiro item selecionado, a quantidade é 1
+
+    numeroCarrinho.innerHTML = qtdProdutosSelecionados
+    exibirNumeroCarrinho()
+    // Exibe o número 1 lá no carrinho
+
+    localStorage.setItem('numero_display_carrinho', qtdProdutosSelecionados)
+    // Armazena no local storage o numero que está lá no carrinho
   }
 }
 
 function limparCarrinho() {
   localStorage.removeItem('db_produtosSelecionados')
+  localStorage.removeItem('numero_display_carrinho')
 
   numeroCarrinho.innerHTML = 0
-  numeroCarrinho.style.display = 'block'
+  exibirNumeroCarrinho()
 }
