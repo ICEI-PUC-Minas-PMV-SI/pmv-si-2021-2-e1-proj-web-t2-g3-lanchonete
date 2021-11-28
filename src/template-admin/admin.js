4af99930f0ef42e9a801ad29a835b881e92d1c32
William <williamxavier2000@hotmail.com>
2021-11-28 17:47:00 -0300

    commit menu lateral

---
+++ src/template-admin/admin.js
@@ -0,0 +1,10 @@
+function mostrarMenu(){
+    document.getElementById('menuLateral').style.width = '180px';
+    document.getElementById('teste').style.marginLeft = '185px';
+    document.getElementById('cabecalho').style.marginLeft = '185px';
+}
+function fecharMenu(){
+    document.getElementById('menuLateral').style.width = '0px';
+    document.getElementById('teste').style.marginLeft = '0px';
+    document.getElementById('cabecalho').style.marginLeft = '0px';
+}
