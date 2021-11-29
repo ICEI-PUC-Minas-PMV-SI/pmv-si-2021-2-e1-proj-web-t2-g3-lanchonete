2c94550282fb608cb1fd2b0e6142b816a293ab2b
brunalourenco <brunalourenso@gmail.com>
2021-11-29 20:49:52 -0300

    alteracaoes html, css e js

--- src/Páginas-Cliente/JS/meusdados.js
+++ src/Páginas-Cliente/JS/meusdados.js
@@ -1,67 +1,163 @@
-//buscar usuario logado foi feito no login
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-//armazena valor do id no do cliente no HTML
-let nome = document.getElementById('nomeUsuario')
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Comandas</title>
 
-//armazena valores dos ids de cadastro do cliente no HTML
+  <link rel="stylesheet" href="../CSS/comandas.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Slab:wght@700&display=swap"
+    rel="stylesheet" />
+</head>
 
-let nome = document.getElementById('nome')
-let nascimento = document.getElementById('nascimento')
-let sexo = document.querySelector('input[name=sexo]:checked')
-let cpf = document.getElementById('cpf')
-let rua = document.getElementById('rua')
-let numero = document.getElementById('numero')
-let bairro = document.getElementById('bairro')
-let complemento = document.getElementById('complemento')
-let referencia = document.getElementById('referencia')
-let cep = document.getElementById('cep')
-let cidade = document.getElementById('cidade')
-let telefone = document.getElementById('telefone')
-let email = document.getElementById('email')
-let confirmarEmail = document.getElementById('emailConfirmar')
-let senha = document.getElementById('senha')
+<body>
 
-// busca dados do cliente no local storage e converte os valores
-var dadosUsuario = localStorage.getItem('usuarioCorrente');
-let mostrar = JSON.parse(dadosUsuario)
+  <!--Início Template Padrão-->
 
-//armazena no localstorage a id do usuario
-usuarioCorrente.id = usuario.id
+  <div class="container">
+    <header>
+      <div class="menu">
+        <div class="logo">
+          <a href="#">
+            <img src="../midia/logo-lanchonete.png" alt="Logo Lanchonete" class="logo" />
+          </a>
+        </div>
+        <div class="botoes">
+          <ul>
+            <li>
+              <a href="../Html/Perfil-admin.html">
+                <img src="../midia/perfil-icon.png" alt="Ícone Perfil" class="icon" />
+                <p>Perfil</p>
+              </a>
+            </li>
+            <li>
+              <a href="Financeiro.html">
+                <img src="../midia/financeiro-icon.png" alt="Ícone Financeiro" class="icon" />
+                <p>Financeiro</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../midia/cardapio-icon.png" alt="Ícone Cardápio" class="icon" />
+                <p>Cardápio</p>
+              </a>
+            </li>
+            <li>
+              <a href="comandas.html">
+                <img src="../midia/comandas-icon.png" alt="Ícone Comandas" class="icon" />
+                <p>Comandas</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+       </div>
+    </header>
 
-//armazena os dados 
-email.value = mostrar.email
-confirmarEmail.value = mostrar.confirmarEmail
-nome.value = mostrar.nome
-senha.value = mostrar.senha
-cpf.value = mostrar.cpf
-telefone.value = mostrar.telefone
-nascimento.value = mostrar.nascimento
-sexo.value = mostrar.sexo
-cep.value = mostrar.cep
-cidade.value = mostrar.cidade
-rua.value = mostrar.rua
-numero.value = mostrar.numero
-bairro.value= mostrar.bairro
-complemento.value = mostrar.complemento
-referencia.value = mostrar.referencia
+      <!--Final Template Padrão-->
+
+    <div class="conteudo">
+      <body>
+        <h1 id="titulo" >Comandas</h1>
+
+          <div id="pesquisa-cliente">
+            <input type="text" placeholder="Pesquise por um cliente"/>
+            <button type="submit">
+              <img src= "../midia/comandas/search.icon.png" alt="icone-pesquisa"/>
+            </button>
+          </div>
+
+          <div id="data">
+            <input type="date" placeholder="Data"/>
+            <button type="date">
+              <img src= "../midia/comandas/search.icon.png" alt="icone-pesquisa"/>
+            </button>
+          </div>
+
+       <div class="tabela">
+         <table>
+         <tr id="nome-colunas" >
+           <th id="coluna01" >Comanda/descrição</th>
+           <th id="coluna02" >Cliente</th>
+           <th id="coluna03" >Data</th>
+           <th id="coluna04" >Valor/pagamento</th>
+           <th id="coluna05" >Status</th>
+         </tr>
+
+         <tr>
+          <td>01. 1 X-Tudo, 1 Guaraná 350 ml <br> Obs: Sem milho verde.</td>
+          <td>Júlia Rodrigues Alves</td>
+          <td>20/10/2021</td>
+          <td>R$ 45,00 <br> Dinheiro <br> Troco para R$50,00 </td>
+          <td>
+           <form>
+            <label for=""></label>
+            <select name="" id="select">
+             <option value="">SAIU PARA ENTREGA</option>
+             <option value="">EM PRODUÇÃO</option>
+             <option value="">PENDENTE</option>
+            </select>
+           </form>
+         </td> 
+         </tr>
+         <tr>
+           <td>02. 1 X-Cheese, 1 Coca 350 ml <br> Obs: Sem milho verde.</td>
+             <td>Antônio Augusto Souza</td>
+             <td>20/10/2021</td>
+             <td>R$ 60,00 <br> Cartão</td>
+             <td>
+              <form>
+               <label for=""></label>
+               <select name="" id="select">
+                <option value="">SAIU PARA ENTREGA</option>
+                <option value="">EM PRODUÇÃO</option>
+                <option value="">PENDENTE</option>
+               </select>
+              </form>
+            </td>    
+         </tr>
+         <tr>
+          <td>03. 1 X-Burguer, 1 Suco natural 500ml <br> Obs: Sem tomate.</td>
+            <td>Marco Aurélio</td>
+            <td>20/10/2021</td>
+            <td>R$ 50,00 <br> Dinheiro</td>
+            <td>
+             <form>
+              <label for=""></label>
+              <select name="" id="select">
+               <option value="">SAIU PARA ENTREGA</option>
+               <option value="">EM PRODUÇÃO</option>
+               <option value="">PENDENTE</option>
+              </select>
+             </form>
+           </td>    
+        </tr>
+        <tr>
+          <td>04. 1 X-Tudo, 1 Fanta 350 ml <br> Obs: Sem milho verde.</td>
+          <td>Camila Cassia</td>
+          <td>20/10/2021</td>
+          <td>R$ 40,00 <br> Dinheiro</td>
+          <td>
+           <form>
+            <label for=""></label>
+            <select name="" id="select">
+             <option value="">SAIU PARA ENTREGA</option>
+             <option value="">EM PRODUÇÃO</option>
+             <option value="">PENDENTE</option>
+            </select>
+           </form>
+         </td> 
+         </tr>
+
+         
+         </table>
+       </div>
 
 
-//Logout e voltar para pagina index
-alert('Retornando para a página inicial')
-function logout() {
-    if (usuarioCorrente.id == usuario.id) {
-        dadosUsuario = localStorage.clear();
-        window.location.href = "../../Páginas-Cliente/Html/Index.html"
-    }
-}
-
-//Excluir dados do cliente
-
-alert('Os dados do(a) usuário(a) foram apagados com sucesso !')
-
-function excluirUsuario() {
-    if (usuarioCorrente.id == usuario.id) {
-        localStorage.removeItem('db_usuarios.usuario');
-        window.location.href = '../../Páginas-Cliente/Html/Index.html'
-
-    }
+       </body>
+    </div>
+  </div>
+</body>
