f1427f98711534780524e587910fe9112cb2abc9
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-11-29 23:50:55 -0300

    atualizações

---
+++ src/Páginas-Cliente/JS/carrinho.js
@@ -0,0 +1,7 @@
+const c = (el)=>document.querySelector(el)
+const cs = (el)=>document.querySelectorAll(el)
+
+modelsJson.map((item,index)=>{
+     let valorprodutoqnt = document.querySelector ('qnt, badição, bsubtração').cloneNode(true)
+     document.querySelector('qnt, badição, bsubtração').append(valorprodutoqnt);
+});
