6ef15e03da7d945c0280c9a7a759c929e85e1d76
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-01 21:25:39 -0300

    atualizações

--- src/Páginas-Cliente/JS/scripcarrinho.js
+++
@@ -1,8 +0,0 @@
-const c = (el)=> document.querySelector(el);
-const cs = (el)=> document.querySelectorAll(el);
-
-modelsJson.map((item, index)=>{
-    let quantidade = c('#quantidade, #bsubtração, #badição').cloneNode(true);
-    document.querySelector('#quantidade').append('#bsubtração, #badição')
-    
-})
