c2a5ae5d50beb37a867272921651a253244b7f34
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-07 20:33:10 -0300

    atualizações

--- src/Páginas-Cliente/JS/pagamento.js
+++ src/Páginas-Cliente/JS/pagamento.js
@@ -1,52 +1,27 @@
-//var n1 = Number (window.prompt('Digite um número: '))
-//var n2 = Number (window.prompt('Digite outro número: '))
-//var s = n1 + n2 
+if (localStorage.getItem('db_produtosSelecionados')) {
+    let produtosSelecionados = JSON.parse(
+      localStorage.getItem('db_produtosSelecionados')
+    )
+    let listaProdutos = document.getElementById('resumop')
+    listaProdutos.innerHTML = ''
+  
+    let precos = []
+  
+    for (produto of produtosSelecionados) {
+      listaProdutos.innerHTML += `
+      <div id="resumop">
+      <p id="descriçãop" >${produto.nomeProduto}</p><p id="preçop" >R$${produto.precoProduto}</p>
+    </div>
+      `
+      precos.push(parseFloat(produto.precoProduto).toFixed(2))
+    }
+  
+    let soma = 0
+    for (preco of precos) {
+      soma += parseFloat(preco)
+    }
+  
+    let valorTotal = document.getElementById('valor-total')
+    valorTotal.innerHTML = 'R$' + soma.toFixed(2)
 
-//Number = transforma em número
-//Number.parseFloat (numero real-com virgula)
-//Number.parseInt (numero inteiro)
-
-//window.alert('A soma dos valores é ' + String(s))
-//String(s)
-
-
-// var nome = window.prompt('Qual é seu nome?')
-// document.write(`Seu nome tem ${nome .length} letras.`)
-// placeholder = `${var}`
-// var.length = diz quantos caracteres a string tem
-// var.toUpperCase() = joga tudo para maiusculas
-// var.toLowercase() = joga tudo para minusculas
-
-
-// +  = soma
-// -  = subtração
-// *  = multiplicação
-// /  = divisão
-// %  = sobra da divisão
-// ** = potência
-
-// ==  ver se é igual
-// === ver se é identico
-
-
-// !   negação
-// &&  conjunção
-// || disjunção
-
-
-
-// ordem de precedência dos operadores =
-// ()         **         * / %       + -
-
-// n = 8
-// n += 2  = 10
-
-
-
-
-
-
-//var n1 = 1545.50
-//colocar com duas casas decimais                                      n2.tofixed(2)
-//colocar com vírgula                                                  n1.toFixed(2).replace('.' ',')
-//mudar para real ou outra moeda        n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+  }
