59a0490c3801285c66351e494e90d1e70c063ec4
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-07 16:23:10 -0300

    marcações

--- src/Páginas-Cliente/JS/pagamento.js
+++ src/Páginas-Cliente/JS/pagamento.js
@@ -1,12 +1,49 @@
 //var n1 = Number (window.prompt('Digite um número: '))
 //var n2 = Number (window.prompt('Digite outro número: '))
 //var s = n1 + n2 
+
+//Number = transforma em número
+//Number.parseFloat (numero real-com virgula)
+//Number.parseInt (numero inteiro)
+
 //window.alert('A soma dos valores é ' + String(s))
 //String(s)
 
 
-//var nome = window.prompt('Qual é seu nome?')
-//document.write(`Seu nome tem ${nome .length} letras.`)
+// var nome = window.prompt('Qual é seu nome?')
+// document.write(`Seu nome tem ${nome .length} letras.`)
+// placeholder = `${var}`
+// var.length = diz quantos caracteres a string tem
+// var.toUpperCase() = joga tudo para maiusculas
+// var.toLowercase() = joga tudo para minusculas
+
+
+// +  = soma
+// -  = subtração
+// *  = multiplicação
+// /  = divisão
+// %  = sobra da divisão
+// ** = potência
+
+// ==  ver se é igual
+// === ver se é identico
+
+
+// !   negação
+// &&  conjunção
+// || disjunção
+
+
+
+// ordem de precedência dos operadores =
+// ()         **         * / %       + -
+
+// n = 8
+// n += 2  = 10
+
+
+
+
 
 
 //var n1 = 1545.50
