2c2ea96f928be1f3a1dc10b0ba4756b696bd2f49
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-07 00:33:54 -0300

    atualizações

---
+++ src/Páginas-Cliente/JS/pagamento.js
@@ -0,0 +1,15 @@
+//var n1 = Number (window.prompt('Digite um número: '))
+//var n2 = Number (window.prompt('Digite outro número: '))
+//var s = n1 + n2 
+//window.alert('A soma dos valores é ' + String(s))
+//String(s)
+
+
+//var nome = window.prompt('Qual é seu nome?')
+//document.write(`Seu nome tem ${nome .length} letras.`)
+
+
+var n1 = 1545.50
+//colocar com duas casas decimais                                      n2.tofixed(2)
+//colocar com vírgula                                                  n1.toFixed(2).replace('.' ',')
+//mudar para real ou outra moeda        n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
