452a8e5376cb31f00f3cfb54514469acf6e151a7
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-07 22:26:15 -0300

    atualizações

--- src/Páginas-Cliente/JS/pagamento.js
+++ src/Páginas-Cliente/JS/pagamento.js
@@ -10,7 +10,8 @@ if (localStorage.getItem('db_produtosSelecionados')) {
     for (produto of produtosSelecionados) {
       listaProdutos.innerHTML += `
       <div id="resumop">
-      <p id="descriçãop" >${produto.nomeProduto}</p><p id="preçop" >R$${produto.precoProduto}</p>
+      <p id="descriçãop">${produto.nomeProduto}</p>
+      <p id="preçop">R$${produto.precoProduto}</p>
     </div>
       `
       precos.push(parseFloat(produto.precoProduto).toFixed(2))
@@ -21,7 +22,44 @@ if (localStorage.getItem('db_produtosSelecionados')) {
       soma += parseFloat(preco)
     }
   
-    let valorTotal = document.getElementById('valor-total')
+    let valorTotal = document.getElementById('valortotal02')
+    valorTotal.innerHTML = 'R$' + soma.toFixed(2)
+
+  }
+
+
+  
+  
+
+
+
+
+
+  if (localStorage.getItem('db_produtosSelecionados')) {
+    let produtosSelecionados = JSON.parse(
+      localStorage.getItem('db_produtosSelecionados')
+    )
+    let listaProdutos = document.getElementById('lista-produtos')
+    listaProdutos.innerHTML = ''
+  
+    let precos = []
+  
+    for (produto of produtosSelecionados) {
+      listaProdutos.innerHTML += `
+      <div id="resumop">
+      <p id="descriçãop">${produto.nomeProduto}</p>
+      <p id="preçop">R$${produto.precoProduto}</p>
+    </div>
+      `
+      precos.push(parseFloat(produto.precoProduto).toFixed(2))
+    }
+  
+    let soma = 0
+    for (preco of precos) {
+      soma += parseFloat(preco)
+    }
+  
+    let valorTotal = document.getElementById('valortotal02')
     valorTotal.innerHTML = 'R$' + soma.toFixed(2)
 
   }
