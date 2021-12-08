bca00285eefddd002dfa15e896f1be70f13f2d5e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2021-12-08 19:02:11 -0300

    atualizações

--- src/Páginas-Cliente/JS/addcartao.js
+++ src/Páginas-Cliente/JS/addcartao.js
@@ -1,40 +1,24 @@
-function abrirModalLogin(modalID1) {
-    let modal = document.getElementById(modalID1)
-    modal.style.display = 'block'
-  
-    // DESFOCAR E ESCURECER FUNDO QUANDO ABRE O MODAL
-    let filterBlur = document.getElementsByClassName('filter-blur')
-    for (elemento of filterBlur) {
-      // console.log(elemento)
-      elemento.style.filter = 'blur(3px) brightness(75%)'
-    }
-  }
-  
-  function fecharModalLogin(modalID) {
-    let modal = document.getElementById(modalID)
-    modal.style.display = 'none'
-  
-    // TIRAR DESFOQUE DO FUNDO QUANDO FECHA O MODAL
-    let filterBlur = document.getElementsByClassName('filter-blur')
-    for (elemento of filterBlur) {
-      // console.log(elemento)
-      elemento.style.filter = 'none'
-    }
-  }
-  
-  function abrirModal(modalID1, modalID2) {
-    let modal = document.getElementById(modalID1)
-    modal.style.display = 'block'
-  
-    let modal2 = document.getElementById(modalID2)
-    modal2.style.display = 'none'
-  }
-  
-  function fecharModal(modalID, modalID2) {
-    let modal = document.getElementById(modalID)
-    modal.style.display = 'none'
-  
-    let modal2 = document.getElementById(modalID2)
-    modal2.style.display = 'block'
-  }
-  
+function cadastroCartao(){
+  var numerocartao = document.getElementById("numero")
+  var validadecartao = document.getElementById("validade")
+  var cvccartao = document.getElementById("cvc")
+  var nomecartao1 = document.getElementById("nomecartao")
+  var cpfcartao = document.getElementById("cpftitular")
+
+  var dadoscartao = JSON.parse(localStorage.getItem("DadosCartao"))
+
+if(dadoscartao == null) {
+  localStorage.setItem("DadosCartao", "[]");
+  dadoscartao = [];
+}
+ var auxRegistro = {
+   numero: numero.value,
+   validade: validade.value,
+   cvc: cvc.value,
+   nome: nomecartao1.value,
+   cpf:cpftitular.value
+ }
+
+  dadoscartao.push(auxRegistro);
+  localStorage.setItem("dadoscartao", JSON.stringify(dadoscartao));
+}
